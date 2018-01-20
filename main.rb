c79c16ea2d2985ab5fee20f3e7081bc44727b6ba
Animesh <hello@animesh.ltd>
2018-01-21 01:30:27 +0530

    Updated license

--- main.rb
+++ main.rb
@@ -6,9 +6,13 @@
 ##  statistics, and prints out the results for the user.
 ##
 ##  Created on January 21, 2018 by Animesh Mishra <hello@animesh.ltd>
-##
-##  © 2018 Animesh Mishra
+##  © 2018 Animesh Ltd
+##  
 ##  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except 
 ##  in compliance with the License bundled with the application.
 ##
-
+##  Unless required by applicable law or agreed to in writing, software distributed under the License 
+##  is distributed on an "as is" basis without warranties or conditions of any kind, either express or 
+##  implied. See the License for the specific language governing permissions and limitations under the 
+##  License.
+##
